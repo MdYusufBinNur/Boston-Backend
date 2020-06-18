5f4b0ca0c0a7a4531f70b898143101a3afda16ea
MdYusufBinNur <engr.mdyusuf@gmail.com>
2020-06-18 10:38:33 +0600

    Message Model

---
+++ models/Message.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const MessageSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'user',
+        required: true
+    },
+    receiver: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'user',
+        required: true
+    },
+    message_text:{
+        type: String
+    },
+    message_file:{
+        type: String
+    }
+});
+
+module.exports = Message = mongoose.model('message',MessageSchema);
